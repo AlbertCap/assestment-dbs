713814fcfcc32ff77025f6ab26d8e2a537111336
AlbertCap <albert.phonbut@gmail.com>
2026-07-17 13:04:52 +0700

    query4-mongodb

--- 02_querying_assessment/mongodb/query_task4.mongodb.js
+++ 02_querying_assessment/mongodb/query_task4.mongodb.js
@@ -8,7 +8,23 @@
 
 // Bonus: The dataset is identical in the PostgreSQL database, meaning the same business insight can be retrieved.
 // Write the equivalent query for PostgreSQL. See query_task4_bonus.sql
+use("chrome-burger-db");
 
+db.orders.aggregate([
+    {
+      $group: {
+        _id: null,
+        total_revenue: {
+          $sum: "$total_price"
+        }
+      }
+    },{
+      $project: {
+        _id: 0,
+        total_revenue: 1
+      }
+    }
+]);
 // ---------------------------------------------------------------
 // Your thinking process (required)
 // ---------------------------------------------------------------
@@ -17,5 +33,9 @@
 // are involved, and what MongoDB concepts you plan to use.
 // Write in English or Thai. Do not skip this step.
 //
-// Your thinking:
-//
+// Your thinking: ข้อนี้ต้องคิดตรงที่ต้องใช้ Aggregate ผมเลยเริ่มอ่านจากตัวอย่างเก่า โดยลองเริ่มใช้ $match ก่อน
+// แต่ match มักจะใช้กับ document ที่มี status แต่ของ orders เราไม่มีผมเลยลองหาทางเลือกอื่นๆ
+// เลยใช้ group แทนซึ่งทำให้เราสร้าง document total_revenue ที่ใช้คำนวณ SUM ได้เหมือนกัน
+// อีกอย่างผมลองใส่ _id เป็น null ทดลองดูเพราะอยากได้ทุก document แต่สุดท้ายก็ได้คำตอบออกมาครับ
+// อีกปัญหานึงที่ติดคือการแสดงแค่ค่า total_revenue ไม่สามารถใช้วิธีเดียวกับ find ได้
+// เลยลองดูวิธีจนเจอว่าเราใช้ project ในการเลือกแสดงได้
